f79c04baa4d372b6b2b325e05a17c8a15f155fd8
Zhi Li <zhili@utexas.edu>
2024-07-15 16:12:44 +0800

    allow downloading kokkos kernels using bash

--- scripts/get_kokkos.sh
+++ scripts/get_kokkos.sh
@@ -2,7 +2,7 @@
 
 # when changing dependency versions,
 # make sure that the related paths in the Makefile are still valid.
-kokkos_ver="3.7.01"
+kokkos_ver="4.2.01"
 
 get_kokkos() {
 	local ver="$1"
@@ -15,4 +15,16 @@ get_kokkos() {
 	fi
 }
 
+get_kokkoskernels() {
+	local ver="$1"
+	# clone KOKKOS Kernels, if no kokkos dir exists
+	if [ -d "kokkos-kernels" ]; then
+		echo "Existing directory \"kokkos-kernels\" found. Skipping download..."
+	else
+		echo "Cloning kokkos-kernels..."
+		git clone --depth 1 --branch $ver https://github.com/kokkos/kokkos-kernels.git
+	fi
+}
+
 get_kokkos "$kokkos_ver"
+get_kokkoskernels "$kokkos_ver"
