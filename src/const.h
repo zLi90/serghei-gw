e2077933da19ec4a669ff4089eee8b016706a7aa
Zhi Li <licheeeeee@gmail.com>
2024-07-16 13:10:51 +0800

    use pointwise friction by default

--- src/const.h
+++ src/const.h
@@ -66,7 +66,7 @@
 #endif
 
 //friction model (0-->upwind or 1-->pointwise-centered)
-#define POINTWISE_FRICTION 0
+#define POINTWISE_FRICTION 1
 
 #define SERGHEI_FRICTION_MANNING 1
 #define SERGHEI_FRICTION_DARCYWEISBACH 2
